7ae5b20c795a7117ce824c8c4ebd7fc666d14804
toure <tourekadiy@gmail.com>
2017-07-07 12:02:38 +0200

    agencement d'articles en colonnes

--- index.php
+++ index.php
@@ -16,13 +16,15 @@ foreach ($array as $file) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"
     <title>My Awesome Blog</title>
 </head>
-<body>
-    <h1>My Awesome Blog</h1>
-    <nav><a href="create.html">New Post</a></nav>
+<body class="container">
+    <div class="row"><h1 class="col-xs-6 col-xs-offset-3">My Awesome Blog</h1>
+    <div class="row"><nav><a class="col-sm-2 col-sm-offset" href="create.html">New Post</a></nav>
+    <div class="container">
     <?php foreach ($files as $file) { ?>
-    <article>
+     <article class="col-sm-3 col-sm-offset-2 col-xs-6 col-xs-offset-3">
         <h2><?php echo basename($file, '.txt'); ?></h2>
         <p><?php echo file_get_contents('post/' . $file); ?></p>
         <form action="delete-post.php" method="POST">
